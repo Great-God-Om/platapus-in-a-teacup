801cc11fd7087ecc68c2f9bec32979a4181805c5
Luke Lighthart <Cu-Sith@gmx.com>
2021-08-06 16:44:44 -0400

    Prettier Format

--- src/Components/Navbar/index.tsx
+++ src/Components/Navbar/index.tsx
@@ -8,11 +8,31 @@ export default function Navbar(): JSX.Element {
 	}, [])
 	return (
 		<Menu pointing secondary>
-			<Menu.Item name="Shop" onClick={clickHandler} active={currentPage === 'Shop'}/>
-			<Menu.Item name="About" onClick={clickHandler} active={currentPage === 'About'}/>
-			<Menu.Item name="Shipping" onClick={clickHandler} active={currentPage === 'Shipping'}/>
-			<Menu.Item name="FAQ" onClick={clickHandler} active={currentPage === 'FAQ'}/>
-			<Menu.Item name="Contact" onClick={clickHandler} active={currentPage === 'Contact'}/>
+			<Menu.Item
+				name="Shop"
+				onClick={clickHandler}
+				active={currentPage === 'Shop'}
+			/>
+			<Menu.Item
+				name="About"
+				onClick={clickHandler}
+				active={currentPage === 'About'}
+			/>
+			<Menu.Item
+				name="Shipping"
+				onClick={clickHandler}
+				active={currentPage === 'Shipping'}
+			/>
+			<Menu.Item
+				name="FAQ"
+				onClick={clickHandler}
+				active={currentPage === 'FAQ'}
+			/>
+			<Menu.Item
+				name="Contact"
+				onClick={clickHandler}
+				active={currentPage === 'Contact'}
+			/>
 		</Menu>
 	)
 }
