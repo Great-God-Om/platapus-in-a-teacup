61d6b2ac78f6be9acda54c71384a8980be0d9d30
Luke Lighthart <Cu-Sith@gmx.com>
2021-08-06 16:44:10 -0400

    Made menu functional

--- src/Components/Navbar/index.tsx
+++ src/Components/Navbar/index.tsx
@@ -1,9 +1,18 @@
+import { useCallback, useState } from 'react'
 import { Menu } from 'semantic-ui-react'
 
 export default function Navbar(): JSX.Element {
+	const [currentPage, setCurrentPage] = useState<string>('Shop')
+	const clickHandler = useCallback((_, { name }) => {
+		setCurrentPage(name)
+	}, [])
 	return (
 		<Menu pointing secondary>
-			<Menu.Item name="Home" />
+			<Menu.Item name="Shop" onClick={clickHandler} active={currentPage === 'Shop'}/>
+			<Menu.Item name="About" onClick={clickHandler} active={currentPage === 'About'}/>
+			<Menu.Item name="Shipping" onClick={clickHandler} active={currentPage === 'Shipping'}/>
+			<Menu.Item name="FAQ" onClick={clickHandler} active={currentPage === 'FAQ'}/>
+			<Menu.Item name="Contact" onClick={clickHandler} active={currentPage === 'Contact'}/>
 		</Menu>
 	)
 }
