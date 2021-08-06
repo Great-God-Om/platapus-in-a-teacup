2af4e67e1513f7aa10f9e0fcfd04f38573ffde76
Luke Lighthart <Cu-Sith@gmx.com>
2021-08-06 16:32:02 -0400

    Project Setup

---
+++ src/Components/Navbar/index.tsx
@@ -0,0 +1,12 @@
+import { Menu } from 'semantic-ui-react'
+
+export default function Navbar(): JSX.Element {
+    return (
+        <Menu
+            pointing
+            secondary>
+                <Menu.Item 
+                    name="Home" />
+        </Menu>
+    )
+}
