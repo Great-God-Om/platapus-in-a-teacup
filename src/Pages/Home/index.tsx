55c3687241adbe765d91181bfd39a2c6fc2732d1
Luke Lighthart <Cu-Sith@gmx.com>
2021-08-16 10:44:47 -0400

    Added favicon

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -3,14 +3,22 @@ import { Button, Card, Image } from 'semantic-ui-react'
 import './home.scss'
 import firebase from '../../Firebase/firebase'
 import { useEffect, useState } from 'react'
+
+interface product {
+	name: string
+	description: string
+	url: string
+	img_src: string
+}
+
 export default function Home() {
-	const [products, setProducts] = useState<any[]>([])
+	const [products, setProducts] = useState<product[]>([])
 	useEffect(() => {
 		return firebase
 			.firestore()
 			.collection('products')
 			.onSnapshot(col => {
-				setProducts(col.docs.map(doc => doc.data()))
+				setProducts(col.docs.map(doc => doc.data() as product))
 			})
 	}, [])
 	return (
